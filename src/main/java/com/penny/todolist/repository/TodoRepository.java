e872057b12f7d29f17a422626b1c53b46364fcde
penny <925580783@qq.com>
2020-08-06 11:00:43 +0800

    [spring-boot-todo-list][penny]feature: implement should_return_Todos_when_get_Todos_given_none test

---
+++ src/main/java/com/penny/todolist/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.penny.todolist.repository;
+
+import com.penny.todolist.model.Todo;
+
+import java.util.List;
+
+public class TodoRepository {
+    public List<Todo> findAll() {
+        return null;
+    }
+}
---
+++ src/main/java/com/penny/todolist/service/TodoService.java
@@ -0,0 +1,17 @@
+package com.penny.todolist.service;
+
+import com.penny.todolist.model.Todo;
+import com.penny.todolist.repository.TodoRepository;
+
+import java.util.List;
+
+public class TodoService {
+    private TodoRepository todoRepository;
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+}
