73a492b89135ba78407162090f377e7680d98790
penny <925580783@qq.com>
2020-08-06 11:13:56 +0800

    [spring-boot-todo-list][penny]feature: implement should_return_Todo_when_post_Todo_given_Todo test

--- src/test/java/com/penny/todolist/service/TodoServiceTest.java
+++ src/test/java/com/penny/todolist/service/TodoServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -25,5 +25,18 @@ public class TodoServiceTest {
         List<Todo> todoList = todoService.getTodos();
         //then
         assertIterableEquals(todoList, todos);
+    }
+
+    @Test
+    public void should_return_Todo_when_post_Todo_given_Todo() {
+        //given
+        TodoRepository todoRepository = mock(TodoRepository.class);
+        Todo todo = new Todo(1,"test1",false);
+        given(todoRepository.save(todo)).willReturn(todo);
+        //when
+        TodoService todoService = new TodoService(todoRepository);
+        Todo newtodo = todoService.addTodo(todo);
+        //then
+        assertEquals(newtodo, todo);
     }
 }
